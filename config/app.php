e431c75340c377b0ae8ec6dcad364156600b8ef2
nitinshendkar <nitinshendkar@gmail.com>
2016-07-05 18:57:36 +0530

    commit conf changes

--- config/app.php
+++ config/app.php
@@ -157,7 +157,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Illuminate\Html\HtmlServiceProvider::class
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -204,8 +204,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Form'=> Illuminate\Html\FormFacade::class,
-        'Html'=> Illuminate\Html\HtmlFacade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
 
     ],
